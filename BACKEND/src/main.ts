1c3f60fec4bd90a6f8086e7aaef837aff5222121
HIBA BEGHDI <beghiba@gmail.com>
2024-11-19 11:11:21 +0100

    updated cors

--- BACKEND/src/main.ts
+++ BACKEND/src/main.ts
@@ -2,24 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter,NestFastifyApplication} from '@nestjs/platform-fastify';
 import fastifyMultipart from '@fastify/multipart';
+import fastifyCors from '@fastify/cors';
+
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   app.enableCors({
-    origin: [process.env.FRONTEND_URL || 'http://localhost:3000',
-      'https://quick-connect-dep.vercel.app/'
+    origin: [
+      'https://quick-connect-dep.vercel.app',
+      'http://localhost:3000'
     ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
-  }); 
+    allowedHeaders: ['Content-Type', 'X-Requested-With'],
+    exposedHeaders: ['Content-Range', 'X-Content-Range'],
+  });
 
-  await app.register(fastifyMultipart as any, {
+  
+  await (app as any).register(fastifyMultipart, {
     limits: {
-      fileSize: 5 * 1024 * 1024
+      fileSize: 5 * 1024 * 1024 // 5MB
     }
   });
 
-  // await app.listen(process.env.PORT ?? 3001,'0.0.0.0');
-
+ 
   const port = process.env.PORT || 3001;
   
   await app.listen(port, '0.0.0.0', () => {
